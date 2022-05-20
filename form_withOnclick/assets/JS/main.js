3d12ea2bf74d1f9d1ecc51aed4f1b71bafae0cf7
Simara08 <100687456+Simara08@users.noreply.github.com>
2022-05-20 13:33:04 +0400

    Add files via upload

---
+++ form_withOnclick/assets/JS/main.js
@@ -0,0 +1,16 @@
+
+const but =document.querySelector(".bot")
+let text=document.queryAllSelector("input").value;
+but.onclick=function Myfunction() {
+//    let emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+//     if (text!=undefined) {
+//         if (text>1) {
+//             if (text==emailPattern) {
+                const element = document.getElementById("form");
+                element.remove();
+    //         }
+            
+    //     }
+        
+    // }
+}
